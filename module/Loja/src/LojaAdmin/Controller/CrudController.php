79b71574fc6a2ea07cd69cda45fc298b2cf0ad5f
cassio <cassio.ozarias@gmail.com>
2014-02-18 14:30:52 -0400

    [ADJUST] Adicionando mais uma foto  style da pasta public uma foto para a tela de login e a outra para tela de para menu principal
    Signed-off-by: cassio <cassio.ozarias@gmail.com>
    
    Signed-off-by: cassio <cassio.ozarias@gmail.com>

--- module/Loja/src/LojaAdmin/Controller/CrudController.php
+++ module/Loja/src/LojaAdmin/Controller/CrudController.php
@@ -30,7 +30,7 @@ abstract class CrudController extends AbstractActionController {
 
         $paginator = new Paginator(new ArrayAdapter($list));
         $paginator->setCurrentPageNumber($page);
-        $paginator->setDefaultItemCountPerPage(10);
+        $paginator->setDefaultItemCountPerPage(1);
 
         return new ViewModel(array('data' => $paginator, 'page' => $page));
     }
